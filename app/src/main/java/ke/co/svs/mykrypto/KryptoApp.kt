695cd271501496a0020b41507936646e9b3ac60a
wamae <wamaebenson06@gmail.com>
2022-04-08 06:13:11 +0300

    Add koin into application class.

---
+++ app/src/main/java/ke/co/svs/mykrypto/KryptoApp.kt
@@ -0,0 +1,20 @@
+package ke.co.svs.mykrypto
+
+import android.app.Application
+import ke.co.svs.mykrypto.di.retrofitModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class KryptoApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // start Koin!
+        startKoin {
+            // declare used Android context
+            androidContext(this@KryptoApp)
+            // declare modules
+            modules(retrofitModule)
+        }
+    }
+}
